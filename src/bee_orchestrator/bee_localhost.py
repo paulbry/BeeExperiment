d71824d01cd54664171d8dc33b3a6c0cd8be0beb
Paul <pbryant1@kent.edu>
2018-09-04 14:18:10 -0400

    Added todo notes and removed unused modules

--- src/bee_orchestrator/bee_localhost.py
+++ src/bee_orchestrator/bee_localhost.py
@@ -1,5 +1,3 @@
-# system
-from termcolor import cprint
 # project
 from .bee_cluster import BeeTask
 from .orc_translator import Adapter
@@ -46,8 +44,9 @@ class BeeLocalhostLauncher(BeeTask):
         # TODO: what else ???
 
     def execute_workers(self):
+        # TODO: document and support for inputs / outputs
+        # TODO: identify plan for support variables
         pass
-        # TODO: run worker bees
 
     def execute_base(self):
         # TODO: document
@@ -60,33 +59,3 @@ class BeeLocalhostLauncher(BeeTask):
         self.current_status = 2  # Waiting
         for event in self.event_list:
             event.wait()
-
-    # Task management support functions (private)
-    def __fetch_beefile_value(self, key, dictionary, default=None, quit_err=False,
-                              silent=False):
-        """
-        Fetch a specific key/value pair from the .beefile and
-        raise error is no default supplied and nothing found
-        :param key: Key for value in dictionary
-        :param dictionary: dictionary to be searched
-                            e.g. self.__beefile['task_conf']
-        :param default: Returned if no value found, if None (def)
-                        then error message surfaced
-        :param quit_err: Exit with non-zero (default=False)
-        :param silent: Hide warning message (default=False)
-        :return: Value for key. Data type dependent on beefile,
-                    and no verification beyond existence
-        """
-        try:
-            return dictionary[key]
-        except KeyError:
-            if default is not None and not quit_err:
-                if not silent:
-                    cprint("[" + self._task_id + "] User defined value for ["
-                           + str(key) + "] was not found, default value: "
-                           + str(default) + " used.", self.warning_color)
-                return default
-            else:
-                cprint("[" + self._task_id + "] Key: " + str(key) + " was not found in: " +
-                       str(dictionary), self.error_color)
-                exit(1)
