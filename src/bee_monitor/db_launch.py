14aba7a9591929fd5f54c4abc0076cf2b8f0b3b6
Paul <pbryant1@kent.edu>
2018-09-06 10:25:51 -0400

    Expanded support for specific query and printing results

--- src/bee_monitor/db_launch.py
+++ src/bee_monitor/db_launch.py
@@ -1,4 +1,5 @@
 # system
+import ast
 import sqlite3
 from termcolor import cprint
 
@@ -15,14 +16,30 @@ class LaunchDB(object):
 
     def query_all(self):
         # TODO: document
-        # TODO: expand display
         cmd = "SELECT * FROM launcher"
         cprint("Launcher DB - {}".format(cmd), self._query_color)
         cursor = self.__connect_db()
 
         try:
             cursor.execute(cmd)
-            print(cursor.fetchall())
+            for line in cursor.fetchall():
+                self.__print_all(line)
+        except sqlite3.OperationalError as e:
+            cprint(str(e), self._error_color)
+
+        self.__close_db()
+
+    def query_all_specific(self, index, value):
+        cmd = "SELECT * FROM launcher WHERE {}={}".format(
+            str(index), str(value)
+        )
+        cprint("Launcher DB - {}".format(cmd), self._query_color)
+        cursor = self.__connect_db()
+
+        try:
+            cursor.execute(cmd)
+            for line in cursor.fetchall():
+                self.__print_all(line)
         except sqlite3.OperationalError as e:
             cprint(str(e), self._error_color)
 
@@ -44,6 +61,30 @@ class LaunchDB(object):
 
         self.__close_db()
         return r
+
+    def _clean_dict(self, d, indent=1):
+        for key, value in d.items():
+            if isinstance(value, dict):
+                print('\t' * indent + str(key) + ":")
+                self._clean_dict(value, indent + 1)
+            else:
+                print('\t' * indent + str(key) + ": " + str(value))
+
+    def __print_all(self, line):
+        cprint("\nBeefile Name: {}".format(line[5]), self._query_color)
+        print("JobID: {}".format(line[1]))
+        print("Class: {}".format(line[2]))
+        print("Management System: {}".format(line[3]))
+        print("Status: {}".format(line[4]))
+        print("TimeStamp: {}".format(line[13]))
+        cprint("Beefile", self._query_color)
+        self._clean_dict(ast.literal_eval(line[7]))
+        if line[8] is not None:
+            print("Beeflow Name: {}".format(line[8]))
+            print("Beeflow")
+            self._clean_dict(ast.literal_eval(line[10]))
+        if line[11] is not None:
+            cprint("Error: {}".format(line[11]), "red")
 
     def __connect_db(self):
         self.__db_conn = sqlite3.connect(self._db_full)
