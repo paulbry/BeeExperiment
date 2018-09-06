c6628cc8d1c98433a8c7b891f6cbbf6c70e64643
Paul <pbryant1@kent.edu>
2018-09-06 13:16:31 -0400

    Updated

--- src/bee_monitor/db_launch.py
+++ src/bee_monitor/db_launch.py
@@ -15,52 +15,51 @@ class LaunchDB(object):
         self._query_color = "green"
 
     def query_all(self):
-        # TODO: document
+        """
+        Query launcher table for all entries and print in human-readable form
+        SELECT * FROM launcher
+        """
         cmd = "SELECT * FROM launcher"
-        cprint("Launcher DB - {}".format(cmd), self._query_color)
         cursor = self.__connect_db()
-
-        try:
-            cursor.execute(cmd)
+        if self.execute_query(cursor, cmd):
             for line in cursor.fetchall():
                 self.__print_all(line)
-        except sqlite3.OperationalError as e:
-            cprint(str(e), self._error_color)
-
         self.__close_db()
 
     def query_all_specific(self, index, value):
+        """
+        Query launcher table for all entries and print in human-readable forme
+        SELECT * FROM launcher WHERE <index>=<value>
+        """
         cmd = "SELECT * FROM launcher WHERE {}={}".format(
             str(index), str(value)
         )
-        cprint("Launcher DB - {}".format(cmd), self._query_color)
         cursor = self.__connect_db()
-
-        try:
-            cursor.execute(cmd)
+        if self.execute_query(cursor, cmd):
             for line in cursor.fetchall():
                 self.__print_all(line)
-        except sqlite3.OperationalError as e:
-            cprint(str(e), self._error_color)
-
         self.__close_db()
 
     def query_value(self, index, value, result="*"):
         cmd = "SELECT {} FROM launcher WHERE {}={}".format(
             str(result), str(index), str(value)
         )
-        cprint("Launcher DB - {}".format(cmd), self._query_color)
         cursor = self.__connect_db()
-
-        r = None
-        try:
-            cursor.execute(cmd)
-            r = cursor.fetchone()
-        except sqlite3.OperationalError as e:
-            cprint(str(e), self._error_color)
-
+        if self.execute_query(cursor, cmd):
+            r = cursor.fetchone()[0]
+        else:
+            r = None
         self.__close_db()
         return r
+
+    def execute_query(self, cursor, cmd):
+        cprint("Launcher DB - {}".format(cmd), self._query_color)
+        try:
+            cursor.execute(cmd)
+            return True
+        except sqlite3.OperationalError as e:
+            cprint(str(e), self._error_color)
+            return False
 
     def _clean_dict(self, d, indent=1):
         for key, value in d.items():
