7005310b2add908acf914e34b5950b18956faf36
Paul <39570394+paulbry@users.noreply.github.com>
2018-09-23 22:01:48 -0400

    Updates

--- examples/bee-lanl/blast/slurm/blast-err.sh
+++ examples/bee-lanl/blast/slurm/blast-err.sh
@@ -3,5 +3,5 @@
 BLAST_CH=/var/tmp/blast
 BLAST_OUT=output
 
-ch-run --no-home -b $BLAST_OUT $BLAST_CH -- sh -c "cat /mnt/0/input.fasta.0.err \
+ch-run --no-home -b ${BLAST_OUT} ${BLAST_CH} -- sh -c "cat /mnt/0/input.fasta.0.err \
     /mnt/0/input.fasta.1.err > /mnt/0/output.fasta.err"
--- examples/bee-lanl/blast/slurm/blast-prep.sh
+++ examples/bee-lanl/blast/slurm/blast-prep.sh
@@ -2,10 +2,10 @@
 
 BLAST_OUT=output
 
-rm -rf $BLAST_OUT
-mkdir $BLAST_OUT
+rm -rf ${BLAST_OUT}
+mkdir ${BLAST_OUT}
 
 # Create error files (empty)
-touch $BLAST_OUT/input.fasta.0.err \
-        $BLAST_OUT/input.fasta.1.err \
-        $BLAST_OUT/output.fasta.err
+touch ${BLAST_OUT}/input.fasta.0.err \
+        ${BLAST_OUT}/input.fasta.1.err \
+        ${BLAST_OUT}/output.fasta.err
